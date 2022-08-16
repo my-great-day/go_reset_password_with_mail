7c89198a40ffe3d98347ccfa1627651e3fec27d3
Umar <99081017+my-great-day@users.noreply.github.com>
2022-08-16 15:49:38 +0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+import (
+    "fmt"
+    "net/http"
+)
+type httpHandler struct{
+	message string
+}
+
+func (h httpHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request){
+	fmt.Fprint(resp, h.message)
+}
+
+func main() {
+	h1 := httpHandler{message: "index"}
+	h2 := httpHandler{message: "About"}
+
+	http.Handle("/", h1)
+	http.Handle("/about", h2)
+
+	fmt.Println("Server is listening from http://localhost:8181...")
+    http.ListenAndServe(":8181", nil)
+}
